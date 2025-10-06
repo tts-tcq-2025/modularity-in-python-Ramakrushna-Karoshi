5207fcbad647c4f934d19088a83ad8febf267409
Ramakrushna-Karoshi <karoshiramakrushna@gmail.com>
2025-10-06 10:49:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from color_code import ColorCode
 from color_utils import get_all_pairs
-from color_format import format_pair, format_manual_header, format_manual_footer
+from color_format import (
+    format_pair, format_manual_header, format_manual_footer
+)
 from test_color_code import run_all_tests
-from test_color_utils import run_utils_tests
+from test_code_utils import run_utils_tests
+
 
 def print_color_code_manual() -> None:
     print(format_manual_header())
@@ -10,8 +12,9 @@ def print_color_code_manual() -> None:
         print(format_pair(pair_number, major, minor))
     print(format_manual_footer())
 
+
 if __name__ == '__main__':
     run_all_tests()
     run_utils_tests()
     print_color_code_manual()
-    print('Program execution complete. ✨')
+    print('Program execution complete.')
